221fa992f2241b0d46a8c9b5e8a17b981b014454
Derrick <maxamillion32@gmail.com>
2016-01-23 00:33:51 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,23 +1,85 @@
 
-
 function rateFive(){
 
-	var data = JSON.stringify({
-  		"rate": 5
-	});
+    var data = JSON.stringify({
+        "rate": 5
+    });
 
-	var xhr = new XMLHttpRequest();
-	xhr.withCredentials = true;
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
-	xhr.addEventListener("readystatechange", function () {
-  		if (this.readyState === this.DONE) {
-    		console.log(this.responseText);
-  		}
-	});
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
 
-	xhr.open("PUT", "https://[yourAppId].stamplayapp.com/api/cobject/v1/cobjectId/:id/rate");
-	xhr.setRequestHeader("accept", "application/json");
-	xhr.setRequestHeader("content-type", "application/json");
+    xhr.open("PUT", "https://[yourAppId].stamplayapp.com/api/cobject/v1/cobjectId/:id/rate");
+    xhr.setRequestHeader("accept", "application/json");
+    xhr.setRequestHeader("content-type", "application/json");
 
-	xhr.send(data);
+    xhr.send(data);
+}
+
+function upvote(){
+    var data = JSON.stringify({
+        "type": "upvote"
+    });
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("PUT", "https://[yourAppId].stamplayapp.com/api/cobject/v1/:cobjectId/:id/vote");
+    xhr.setRequestHeader("accept", "application/json");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.send(data);
+}
+
+function downvote(){
+    var data = JSON.stringify({
+        "type": "downvote"
+    });
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("PUT", "https://[yourAppId].stamplayapp.com/api/cobject/v1/:cobjectId/:id/vote");
+    xhr.setRequestHeader("accept", "application/json");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.send(data);
+}
+
+function createObjectComment(){
+
+    var comment = document.getElementById('comment').value;
+
+    var data = JSON.stringify({
+        "text": comment
+    });
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("PUT", "https://[yourappid].stamplayapp.com/api/cobject/v1/:cobjectId/:id/comment");
+    xhr.setRequestHeader("accept", "application/json");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.send(data);
 }
